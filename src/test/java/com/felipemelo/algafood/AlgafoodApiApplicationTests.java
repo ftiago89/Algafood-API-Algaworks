070972f81b77172408eb310c3d343ad767e3da13
Felipe Melo <ftiagomelo89@gmail.com>
2021-03-19 22:33:45 -0300

    Projeto criado

---
+++ src/test/java/com/felipemelo/algafood/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipemelo.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
