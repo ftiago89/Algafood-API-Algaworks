1316a361c43776209ce58fec712396113f5f76ee
Felipe Melo <ftiagomelo89@gmail.com>
2021-03-22 07:32:58 -0300

    Adicao das entidades Kitchen e Restaurant

---
+++ src/main/java/com/felipemelo/algafood/domain/entity/Restaurant.java
@@ -0,0 +1,22 @@
+package com.felipemelo.algafood.domain.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Restaurant {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(name = "delivery_tax")
+    private BigDecimal deliveryTax;
+}
