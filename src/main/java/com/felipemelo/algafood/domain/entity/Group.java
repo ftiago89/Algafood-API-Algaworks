7446726fd7ac43765e77a85818fcd9503d1fec99
Felipe <ftiagomelo89@gmail.com>
2021-06-16 07:10:08 -0300

    refs 6.9 Adicionando entidades User e Group e seus relacionamentos ManyToMany.

---
+++ src/main/java/com/felipemelo/algafood/domain/entity/Group.java
@@ -0,0 +1,28 @@
+package com.felipemelo.algafood.domain.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Group {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "group_permission",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private List<Permission> permissions = new ArrayList<>();
+}
