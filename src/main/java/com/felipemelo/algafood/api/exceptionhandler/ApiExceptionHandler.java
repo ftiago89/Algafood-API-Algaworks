b95b5a68b518fd5b836be43a6039bb8fc929bd04
Felipe <ftiagomelo89@gmail.com>
2021-08-03 07:22:55 -0300

    refs 8.14 Criando handler para excecao de entidade em uso e retirando anotacoes @ResponseStatus das excecoes.

--- src/main/java/com/felipemelo/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/felipemelo/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.felipemelo.algafood.api.exceptionhandler;
 
 import com.felipemelo.algafood.domain.exception.BusinessException;
+import com.felipemelo.algafood.domain.exception.EntityInUseException;
 import com.felipemelo.algafood.domain.exception.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,16 @@ public class ApiExceptionHandler {
                 .build();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBody);
+    }
+
+    @ExceptionHandler(EntityInUseException.class)
+    public ResponseEntity<?> handleEntityInUseException(EntityInUseException e){
+        ErrorBody errorBody = ErrorBody.builder()
+                .dateTime(LocalDateTime.now())
+                .message(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorBody);
     }
 
     @ExceptionHandler(BusinessException.class)
