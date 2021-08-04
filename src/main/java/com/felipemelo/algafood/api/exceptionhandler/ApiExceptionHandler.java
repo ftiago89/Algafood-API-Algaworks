f47b28d95b6f090b69a15dab61a7b2400fe38dcc
Felipe <ftiagomelo89@gmail.com>
2021-08-04 07:07:02 -0300

    refs 8.16 Sobrescrevendo o handleExceptionInternal e utilizando ele em todos os exception handlers.

--- src/main/java/com/felipemelo/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/felipemelo/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,10 +3,12 @@ package com.felipemelo.algafood.api.exceptionhandler;
 import com.felipemelo.algafood.domain.exception.BusinessException;
 import com.felipemelo.algafood.domain.exception.EntityInUseException;
 import com.felipemelo.algafood.domain.exception.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -15,32 +17,39 @@ import java.time.LocalDateTime;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e){
-        ErrorBody errorBody = ErrorBody.builder()
-                .dateTime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request){
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBody);
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(EntityInUseException.class)
-    public ResponseEntity<?> handleEntityInUseException(EntityInUseException e){
-        ErrorBody errorBody = ErrorBody.builder()
-                .dateTime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
+    public ResponseEntity<?> handleEntityInUseException(EntityInUseException ex, WebRequest request){
 
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorBody);
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<?> handleBusinessException(BusinessException e){
-        ErrorBody errorBody = ErrorBody.builder()
-                .dateTime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
+    public ResponseEntity<?> handleBusinessException(BusinessException ex, WebRequest request){
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorBody);
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers
+            , HttpStatus status, WebRequest request) {
+
+        if (body == null){
+            body = ErrorBody.builder()
+                    .dateTime(LocalDateTime.now())
+                    .message(status.getReasonPhrase())
+                    .build();
+        } else if (body instanceof String){
+            body = ErrorBody.builder()
+                    .dateTime(LocalDateTime.now())
+                    .message((String) body)
+                    .build();
+        }
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 }
